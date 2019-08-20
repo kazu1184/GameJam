7fb12c561909696167489e4d513bb0e588b59edf
🐈 🐶 <raiki5100@gmail.com>
2019-08-20 12:48:51 +0900

    C: スタート & ゴール壁ポジション取得関数実装

--- GameJam/Assets/Script/StartGoalPosObj.cs
+++ GameJam/Assets/Script/StartGoalPosObj.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class StartGoalPosObj : MonoBehaviour
 {
-    [SerializeField]
-    GameObject[] m_wall;
+    // [SerializeField]
+    // GameObject[] m_wall;
+
+    Vector3 start_wall;
+    Vector3 goal_wall;
 
     // Start is called before the first frame update
     void Start()
     {
         var random_int_array = Enumerable.Range(1, 13).OrderBy(n => Guid.NewGuid()).Take(2).ToArray();
-        Debug.Log(ary[0]);
-        Debug.Log(ary[1]);
-
-        Debug.Break();
 
         var start_obj_name = String.Format("Wall-{0}", random_int_array[0]);
         var goal_obj_name = String.Format("Wall-{0}", random_int_array[1]);
-        Vector3 start_wall = GameObject.Find(start_obj_name).transform.position;
-        Vector3 goal_wall = GameObject.Find(goal_obj_name).transform.position;
+        start_wall = GameObject.Find(start_obj_name).transform.position;
+        goal_wall = GameObject.Find(goal_obj_name).transform.position;
     }
 
     // Update is called once per frame
